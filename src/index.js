e7fa531f9b13509f613dc5803ccdbf296c76666f
Noah Ross <noah.baine@gmail.com>
2019-01-29 08:37:33 -0800

    updated package files and index file

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import App from "./components/App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from "./reducers";
+import middleware from "./middleware"; 
 
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
+
 console.log(store);
 ReactDOM.render(
-<Provider store={store}><App /></Provider>, document.getElementById('root'));
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
