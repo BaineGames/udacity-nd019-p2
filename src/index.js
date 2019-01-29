85304d29ce0a7cf6034a553b882e27dbd97c500c
Noah Ross <noah.baine@gmail.com>
2019-01-29 08:27:07 -0800

    connected app index.js to redux and store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers";
 
-import App from './components/App';
-// import reducer from './reducers';
-// import middleware from './middleware';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const store = createStore(reducer);
+console.log(store);
+ReactDOM.render(
+<Provider store={store}><App /></Provider>, document.getElementById('root'));
